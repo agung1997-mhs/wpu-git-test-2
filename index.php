0e34893f1c153badc2e6cc49e8bd266e76e21afa
agung1997-mhs <agungkemayoran97@gmail.com>
2021-01-13 20:54:04 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam dolorum officiis repellat! Voluptatem dignissimos quo, pariatur assumenda quasi repellat adipisci nam delectus labore est enim a, excepturi odio. Natus, iusto.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
